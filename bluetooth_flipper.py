245c2c820b25beb259da7b9d94aa0d453513a93d
Amogh <amogh.91@gmail.com>
2018-06-11 16:03:48 -0700

    only flip when lid is opened or closed - supports manual toggle after lid open or close

--- bluetooth_flipper.py
+++ bluetooth_flipper.py
@@ -6,52 +6,58 @@ import re
 import time
 from exceptions import OSError
 
+class Flipper:
+    def __init__(self):
+        self.prev_lid_closed_state = self.is_lid_closed()
 
-def is_lid_closed():
-    ioreg = "ioreg -r -k AppleClamshellState -d 4".split(" ")
-    grep = "grep AppleClamshellState".split(" ")
+    def is_lid_closed(self):
+        ioreg = "ioreg -r -k AppleClamshellState -d 4".split(" ")
+        grep = "grep AppleClamshellState".split(" ")
 
-    ioreg_op = subprocess.Popen(ioreg, stdout=subprocess.PIPE)
-    grep_op = subprocess.check_output(grep, stdin=ioreg_op.stdout)
+        ioreg_op = subprocess.Popen(ioreg, stdout=subprocess.PIPE)
+        grep_op = subprocess.check_output(grep, stdin=ioreg_op.stdout)
 
-    match = re.search(r'\"(.*)\" = (.*)', grep_op)
-    attribute, state = match.group(1), match.group(2)
-    
-    if state == "No":
-        return False
-    else:
-        return True
+        match = re.search(r'\"(.*)\" = (.*)', grep_op)
+        attribute, state = match.group(1), match.group(2)
+        
+        if state == "No":
+            return False
+        else:
+            return True
 
-
-def is_bluetooth_on():
-    try:
-        bluetooth_status = "/usr/local/bin/blueutil -p".split(" ")
-        bluetooth_status_op = subprocess.check_output(bluetooth_status)
-        status = int(bluetooth_status_op.split("\n")[0])
-        return bool(status)
-    except Exception as exception:
-        print "Installing blueutil..."
-        install_bluetooth = "/usr/local/bin/brew install blueutil".split(" ")
-        install_op = subprocess.check_output(install_bluetooth)
-        print install_op
-        return is_bluetooth_on()
-
-
-def flip_bluetooth():
-    bluetooth_on = "/usr/local/bin/blueutil -p 1".split(" ")
-    bluetooth_off = "/usr/local/bin/blueutil -p 0".split(" ")
-    
-    if is_lid_closed():
-        if not is_bluetooth_on():
-            subprocess.check_output(bluetooth_on)
-    else:
-        if is_bluetooth_on():
-            subprocess.check_output(bluetooth_off)
+    def is_bluetooth_on(self):
+        try:
+            bluetooth_status = "/usr/local/bin/blueutil -p".split(" ")
+            bluetooth_status_op = subprocess.check_output(bluetooth_status)
+            status = int(bluetooth_status_op.split("\n")[0])
+            return bool(status)
+        except Exception as exception:
+            print exception.message
+            print "Installing blueutil..."
+            install_bluetooth = "/usr/local/bin/brew install blueutil".split(" ")
+            install_op = subprocess.check_output(install_bluetooth)
+            print install_op
+            return self.is_bluetooth_on()
 
 
-print "Running bluetooth flipper..."
+    def flip_bluetooth(self):
+        bluetooth_on = "/usr/local/bin/blueutil -p 1".split(" ")
+        bluetooth_off = "/usr/local/bin/blueutil -p 0".split(" ")
+        lid_closed_state = self.is_lid_closed()
 
+        if lid_closed_state != self.prev_lid_closed_state:
+            self.prev_lid_closed_state = lid_closed_state
+            if lid_closed_state:
+                if not self.is_bluetooth_on():
+                    subprocess.check_output(bluetooth_on)
+            else:
+                if self.is_bluetooth_on():
+                    subprocess.check_output(bluetooth_off)
 
-while True:
-    flip_bluetooth()
-    time.sleep(3)
+def main():
+    flipper = Flipper()
+    while True:
+        flipper.flip_bluetooth()
+        time.sleep(3)
+
+main()
--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/sh -e
+
+USER=$(whoami)
 plist_path="$1"
 plist_filename=$(basename "$plist_path")
 echo $plist_filename
 install_path="/Users/amoghp/Library/LaunchAgents/$plist_filename"
 
+sudo rm -f /var/log/bluetooth_flipper.error
 sudo touch /var/log/bluetooth_flipper.error
+sudo chown $USER /var/log/bluetooth_flipper.error
+
+sudo rm -f /var/log/bluetooth_flipper.output
 sudo touch /var/log/bluetooth_flipper.output
+sudo chown $USER /var/log/bluetooth_flipper.output
 
 echo "installing launchctl plist: $plist_path --> $install_path"
 sudo cp -f "$plist_path" "$install_path"
